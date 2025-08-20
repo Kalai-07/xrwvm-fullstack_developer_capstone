bc3be1e8dd7269ca1628bdee38b9d039e33d1fbc
Kalai-07 <skalaiselvan592@gmail.com>
2025-08-20 19:54:05 +0530

    new changes

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -7,12 +7,16 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
+
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     # Other fields as needed
+
     def __str__(self):
-        return self.name  
+        return self.name
+
+
 # Return the name as the string representation
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
@@ -22,24 +26,26 @@ class CarMake(models.Model):
 
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(
+        CarMake, on_delete=models.CASCADE
+    )  # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
-        ('SEDAN', 'Sedan'),
-        ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
+        ("SEDAN", "Sedan"),
+        ("SUV", "SUV"),
+        ("WAGON", "Wagon"),
         # Add more choices as required
     ]
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default="SUV")
+    year = models.IntegerField(
+        default=2023, validators=[MaxValueValidator(2023), MinValueValidator(2015)]
+    )
     # Other fields as needed
 
     def __str__(self):
         return self.name  # Return the name as the string representation
+
+
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
 # Car Models, using ForeignKey field)
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,20 +6,30 @@ from . import views
 from django.urls import path, include
 from django.views.generic import TemplateView
 
-app_name = 'djangoapp'
+app_name = "djangoapp"
 urlpatterns = [
     # # path for registration
-
-    path('logout', views.logout_user, name='logout'),
-    path('register', views.register, name='register'),
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
-    path(route='login', view=views.login_user, name='login'),
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
-    path(route='add_review', view=views.add_review, name='add_review'),
-    path('dealers/', views.get_dealerships, name='dealers_alias'),
+    path("logout", views.logout_user, name="logout"),
+    path("register", views.register, name="register"),
+    path(route="get_cars", view=views.get_cars, name="getcars"),
+    path(route="login", view=views.login_user, name="login"),
+    path(route="get_dealers", view=views.get_dealerships, name="get_dealers"),
+    path(
+        route="get_dealers/<str:state>",
+        view=views.get_dealerships,
+        name="get_dealers_by_state",
+    ),
+    path(
+        route="dealer/<int:dealer_id>",
+        view=views.get_dealer_details,
+        name="dealer_details",
+    ),
+    path(
+        route="reviews/dealer/<int:dealer_id>",
+        view=views.get_dealer_reviews,
+        name="dealer_details",
+    ),
+    path(route="add_review", view=views.add_review, name="add_review"),
+    path("dealers/", views.get_dealerships, name="dealers_alias"),
     # path for add a review view
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
