06d036a1479b9bb83e754556f88028cf3cc110f0
Kalaiselvan <115524867+Kalai-07@users.noreply.github.com>
2025-08-28 00:38:48 +0530

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -59,18 +59,18 @@ def initiate():
         },
         {
             "name": "A4",
-            "type": "SUV", 
+            "type": "SUV",
             "year": 2023,
             "car_make": car_make_instances[2]},
         {
             "name": "A5",
-            "type": "SUV", 
+            "type": "SUV",
             "year": 2023,
             "car_make": car_make_instances[2]},
         {
             "name": "A6",
-            "type": 
-            "SUV", "year": 2023,
+            "type":"SUV",
+            "year": 2023,
             "car_make": car_make_instances[2]},
         {
             "name": "Sorrento",
